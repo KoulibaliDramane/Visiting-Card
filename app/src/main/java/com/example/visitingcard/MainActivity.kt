c48f7de3a82f569a2e02fb569da7079f7256d8f8
Dramane Koulibali <dramanekoulibali@outlook.com>
2025-08-26 18:12:13 +0000

    Modification image et background

--- app/src/main/java/com/example/visitingcard/MainActivity.kt
+++ app/src/main/java/com/example/visitingcard/MainActivity.kt
@@ -9,12 +9,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Email
